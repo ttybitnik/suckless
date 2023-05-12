bb00edcd80a9256c20b382b0f2836671954e0283
w0lper <vinicius.moraes@eternodevir.com>
2023-05-11 23:49:52 -0300

    feat(dmenu): update colors

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -4,14 +4,14 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"iosevka medium extended:size=10"
+    	 "iosevka medium extended:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#723E31" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#ebdbb2", "#282828" },
+	[SchemeSel] = { "#fbf1c7", "#98971a" },
+	[SchemeOut] = { "#000000", "#cc241d" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
